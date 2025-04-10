2642736557e7aaa5cbfbee49dd280dae3d393bd6
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2025-04-10 09:32:00 -0500

    Update celery.py

--- project/celery.py
+++ project/celery.py
@@ -22,6 +22,6 @@ app.conf.beat_schedule = {
     },
     'run-sms-payment-task': {
         'task': 'app.tasks.smsPayment',
-        'schedule': crontab(minute=28, hour=9),  # Ejecutar a las 8:00 AM todos los días
+        'schedule': crontab(minute=33, hour=9),  # Ejecutar a las 8:00 AM todos los días
     },
 }
