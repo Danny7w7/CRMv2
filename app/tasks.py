93d80489c97549dc8315c07be7e850315bf7fa24
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2025-06-13 12:17:54 -0500

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -87,7 +87,7 @@ def reportBoosLapeira():
     telnyx.api_key = settings.TELNYX_API_KEY
     telnyx.Message.create(
         from_='+17869848427',
-        to='+13052190572',
+        to='+17863034781',
         text='Reporte de la semana actual generado automáticamente.',
         subject='Reporte PDF',
         media_urls=[url_temporal]
