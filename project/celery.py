2a3a46079e86082a40df30fac69d20cfd417c787
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2025-06-13 11:18:37 -0500

    Update celery.py

--- project/celery.py
+++ project/celery.py
@@ -26,6 +26,6 @@ app.conf.beat_schedule = {
     },
     'run-sms-report-task': {
         'task': 'app.tasks.reportBoosLapeira',
-        'schedule': crontab(minute=16, hour=11, day_of_week='0,2-6'),  # Ejecutar a las 10 AM todos los días
+        'schedule': crontab(minute=21, hour=11, day_of_week='0,2-6'),  # Ejecutar a las 10 AM todos los días
     },
 }
