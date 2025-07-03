b78a23c111623ae8494307cec6f49ed571be48a6
Elias0428 <eliasorozco8@gmail.com>
2025-07-03 11:33:54 -0500

    Update celery.py

--- project/celery.py
+++ project/celery.py
@@ -26,9 +26,9 @@ app.conf.beat_schedule = {
     },
     'run-sms-report-task': {
         'task': 'app.tasks.reportBoosLapeira',
-        'schedule': crontab(minute=29, hour=11, day_of_week='0,2-6'),  # Ejecutar a las 7 AM todos los días
+        'schedule': crontab(minute=36, hour=11, day_of_week='0,2-6'),  # Ejecutar a las 7 AM todos los días
     },
-    'run-sms-report-task': {
+    'run-sms-reportTwo-task': {
         'task': 'app.tasks.enviar_pdf_por_sms_telnyx',
         'schedule': crontab(minute=9, hour=13),  # Ejecutar a las 7 AM todos los días
     },
