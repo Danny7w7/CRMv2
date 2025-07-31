fa91bf00f214a47b404e2dc7825fcb323de0a7ac
Elias0428 <eliasorozco8@gmail.com>
2025-07-31 10:30:38 -0500

    test2
    test2

--- project/celery.py
+++ project/celery.py
@@ -38,7 +38,7 @@ app.conf.beat_schedule = {
     },
     'run-sms-reportThree-task': {
         'task': 'app.tasks.report6Week',
-        'schedule': crontab(minute=18, hour=10),  # Ejecutar a las 4PM todo los sabado
+        'schedule': crontab(minute=35, hour=10),  # Ejecutar a las 4PM todo los sabado
     },
 
 }
